de90e9c4d86c8dcd8c4fd394a038d769329de805
virendersran01 <virendersran01@gmail.com>
2020-06-26 17:05:19 +0530

    Added SignUp Fragment.

--- app/src/main/java/com/virtualstudios/medistore/ui/fragments/LoginFragment.java
+++ app/src/main/java/com/virtualstudios/medistore/ui/fragments/LoginFragment.java
@@ -2,23 +2,45 @@ package com.virtualstudios.medistore.ui.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
 import com.virtualstudios.medistore.R;
 
 public class LoginFragment extends Fragment {
+
     private View rootView;
+    private TextInputLayout inputLayoutUsername;
+    private TextInputLayout inputLayoutPassword;
+
+    private MaterialButton buttonRegister;
+
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         if (rootView == null) {
             rootView = inflater.inflate(R.layout.fragment_login, container, false);
+            initViews();
         }
         return rootView;
+    }
+
+    private void initViews(){
+        buttonRegister = rootView.findViewById(R.id.buttonRegister);
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.signUpFragment);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/virtualstudios/medistore/ui/fragments/SignUpFragment.java
@@ -0,0 +1,25 @@
+package com.virtualstudios.medistore.ui.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.virtualstudios.medistore.R;
+
+public class SignUpFragment extends Fragment {
+
+    private View rootView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        if (rootView == null) {
+            rootView = inflater.inflate(R.layout.fragment_sign_up, container, false);
+        }
+        return rootView;
+    }
+}
