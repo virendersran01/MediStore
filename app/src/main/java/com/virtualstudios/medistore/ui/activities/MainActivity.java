7e8594f31958e6cff170514dec67e139f20858ce
virendersran01 <virendersran01@gmail.com>
2020-07-03 17:40:13 +0530

    Added fragment for add reminder.

--- app/src/main/java/com/virtualstudios/medistore/ui/activities/MainActivity.java
+++ app/src/main/java/com/virtualstudios/medistore/ui/activities/MainActivity.java
@@ -1,16 +1,57 @@
 package com.virtualstudios.medistore.ui.activities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.virtualstudios.medistore.R;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FragmentContainerView fragmentContainerView;
+    private FloatingActionButton fabAddReminder;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentContainerView = findViewById(R.id.fragmentContainer);
+        fabAddReminder = findViewById(R.id.fabAddReminder);
+
+        fabAddReminder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(fragmentContainerView).navigate(R.id.addReminderFragment);
+            }
+        });
+
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager()
+                        .findFragmentById(fragmentContainerView.getId());
+        NavController navController = Objects.requireNonNull(navHostFragment).getNavController();
+
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+                if (destination.getLabel().equals("Add Reminder")){
+                    fabAddReminder.setVisibility(View.GONE);
+                }else {
+                    fabAddReminder.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
     }
 }
