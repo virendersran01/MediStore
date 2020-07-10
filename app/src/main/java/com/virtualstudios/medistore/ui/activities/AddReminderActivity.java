dd090f6c9d553b86ea27a1a20ab88ce2bfa58508
virendersran01 <virendersran01@gmail.com>
2020-07-10 17:55:33 +0530

    working on add reminder form.

--- app/src/main/java/com/virtualstudios/medistore/ui/activities/AddReminderActivity.java
+++ app/src/main/java/com/virtualstudios/medistore/ui/activities/AddReminderActivity.java
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatSpinner;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 import com.virtualstudios.medistore.R;
 
 public class AddReminderActivity extends AppCompatActivity {
 
     private AppCompatSpinner spinner;
+    private ImageView imageAddReminderDate;
+    private TextView textReminderDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +28,25 @@ public class AddReminderActivity extends AppCompatActivity {
 
     private void initViews(){
         spinner = findViewById(R.id.spinner);
+        imageAddReminderDate = findViewById(R.id.imageAddDate);
+        textReminderDate = findViewById(R.id.textReminderDate);
+
+        imageAddReminderDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MaterialDatePicker.Builder<Long> builder = MaterialDatePicker.Builder.datePicker();
+                builder.setTitleText("Reminder Date");
+                MaterialDatePicker<Long> picker = builder.build();
+                picker.show(getSupportFragmentManager(), picker.toString());
+
+                picker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Long>() {
+                    @Override
+                    public void onPositiveButtonClick(Long selection) {
+                        textReminderDate.setText(picker.getHeaderText());
+                    }
+                });
+            }
+        });
 
         initDropDownOption();
     }
