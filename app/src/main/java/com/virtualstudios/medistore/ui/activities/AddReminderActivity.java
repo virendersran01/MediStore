ce54f28760d03c5a9515f3396bd8fb426e3e561b
virendersran01 <virendersran01@gmail.com>
2020-07-10 13:40:50 +0530

    working on add reminder form.

--- app/src/main/java/com/virtualstudios/medistore/ui/activities/AddReminderActivity.java
+++ app/src/main/java/com/virtualstudios/medistore/ui/activities/AddReminderActivity.java
@@ -1,16 +1,32 @@
 package com.virtualstudios.medistore.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatSpinner;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import com.virtualstudios.medistore.R;
 
 public class AddReminderActivity extends AppCompatActivity {
 
+    private AppCompatSpinner spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_reminder);
+        initViews();
+    }
+
+    private void initViews(){
+        spinner = findViewById(R.id.spinner);
+
+        initDropDownOption();
+    }
+
+    private void initDropDownOption(){
+        String[] options = new String[]{"One Day Before", "One Week Before", "One Month Before", "Two Month Before", "Three Month Before"};
+        spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, options));
     }
 }
