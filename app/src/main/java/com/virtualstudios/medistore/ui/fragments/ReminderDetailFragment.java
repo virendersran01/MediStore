0cc54b9d6daff64748f82f8045234315bebde596
virendersran01 <virendersran01@gmail.com>
2020-07-11 15:13:54 +0530

    add new fragment for detail reminder.

---
+++ app/src/main/java/com/virtualstudios/medistore/ui/fragments/ReminderDetailFragment.java
@@ -0,0 +1,24 @@
+package com.virtualstudios.medistore.ui.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.virtualstudios.medistore.R;
+
+
+public class ReminderDetailFragment extends Fragment {
+
+    private View rootView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_reminder_detail, container, false);
+        return rootView;
+    }
+}
