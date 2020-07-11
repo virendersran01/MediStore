449abba2d7549e555c3038f5cc402ddea308e1d1
virendersran01 <virendersran01@gmail.com>
2020-07-11 12:28:24 +0530

    add new fragment for add reminder extra details.

--- app/src/main/java/com/virtualstudios/medistore/ui/activities/AddReminderActivity.java
+++ app/src/main/java/com/virtualstudios/medistore/ui/activities/AddReminderActivity.java
@@ -1,58 +1,17 @@
 package com.virtualstudios.medistore.ui.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatSpinner;
-
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.google.android.material.datepicker.MaterialDatePicker;
-import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.virtualstudios.medistore.R;
 
 public class AddReminderActivity extends AppCompatActivity {
-
-    private AppCompatSpinner spinner;
-    private ImageView imageAddReminderDate;
-    private TextView textReminderDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_reminder);
-        initViews();
     }
 
-    private void initViews(){
-        spinner = findViewById(R.id.spinner);
-        imageAddReminderDate = findViewById(R.id.imageAddDate);
-        textReminderDate = findViewById(R.id.textReminderDate);
-
-        imageAddReminderDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MaterialDatePicker.Builder<Long> builder = MaterialDatePicker.Builder.datePicker();
-                builder.setTitleText("Reminder Date");
-                MaterialDatePicker<Long> picker = builder.build();
-                picker.show(getSupportFragmentManager(), picker.toString());
-
-                picker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Long>() {
-                    @Override
-                    public void onPositiveButtonClick(Long selection) {
-                        textReminderDate.setText(picker.getHeaderText());
-                    }
-                });
-            }
-        });
-
-        initDropDownOption();
-    }
-
-    private void initDropDownOption(){
-        String[] options = new String[]{"One Day Before", "One Week Before", "One Month Before", "Two Month Before", "Three Month Before"};
-        spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, options));
-    }
 }
--- app/src/main/java/com/virtualstudios/medistore/ui/fragments/AddReminderFragment.java
+++ app/src/main/java/com/virtualstudios/medistore/ui/fragments/AddReminderFragment.java
@@ -7,17 +7,50 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 import com.virtualstudios.medistore.R;
 
 
 public class AddReminderFragment extends Fragment {
 
+    private View rootView;
+    private ImageView imageAddReminderDate;
+    private TextView textReminderDate;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_reminder, container, false);
+        rootView = inflater.inflate(R.layout.fragment_add_reminder, container, false);
+        initViews();
+        return rootView;
+    }
+
+    private void initViews(){
+        imageAddReminderDate = rootView.findViewById(R.id.imageAddDate);
+        textReminderDate = rootView.findViewById(R.id.textReminderDate);
+
+        imageAddReminderDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MaterialDatePicker.Builder<Long> builder = MaterialDatePicker.Builder.datePicker();
+                builder.setTitleText("Reminder Date");
+                MaterialDatePicker<Long> picker = builder.build();
+                picker.show(getChildFragmentManager(), picker.toString());
+
+                picker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Long>() {
+                    @Override
+                    public void onPositiveButtonClick(Long selection) {
+                        textReminderDate.setText(picker.getHeaderText());
+                    }
+                });
+            }
+        });
+
+
     }
 }
