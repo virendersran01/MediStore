61b36e147fe2567ea444eab3d06952b9807dc990
virendersran01 <virendersran01@gmail.com>
2020-07-13 12:33:32 +0530

    working on detail fragment.

--- app/src/main/java/com/virtualstudios/medistore/ui/fragments/ReminderDetailFragment.java
+++ app/src/main/java/com/virtualstudios/medistore/ui/fragments/ReminderDetailFragment.java
@@ -9,16 +9,29 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.virtualstudios.medistore.R;
+import com.virtualstudios.medistore.data.models.User;
+import com.virtualstudios.medistore.ui.customviews.AvatarView;
 
 
 public class ReminderDetailFragment extends Fragment {
 
     private View rootView;
-
+    private AvatarView avatarView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_reminder_detail, container, false);
+        initViews();
         return rootView;
+    }
+
+    private void initViews(){
+        avatarView = rootView.findViewById(R.id.avatar);
+        User user = new User();
+        user.setName("Virender Sran");
+        user.setColor(R.color.colorPrimaryDark);
+        user.setAvatarUrl("https://picsum.photos/200/300");
+
+        avatarView.setUser(user);
     }
 }
