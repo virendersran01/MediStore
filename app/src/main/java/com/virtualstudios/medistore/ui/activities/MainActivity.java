747849dd7c161a3d873f86ebecc3582bab8b1e4d
virendersran01 <virendersran01@gmail.com>
2020-07-22 22:07:19 +0530

    added user profile and reminders list fragment.

--- app/src/main/java/com/virtualstudios/medistore/ui/activities/MainActivity.java
+++ app/src/main/java/com/virtualstudios/medistore/ui/activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.virtualstudios.medistore.ui.activities;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentContainerView;
 import androidx.navigation.NavController;
 import androidx.navigation.NavDestination;
@@ -12,8 +13,11 @@ import androidx.navigation.fragment.NavHostFragment;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.virtualstudios.medistore.R;
 
@@ -23,37 +27,67 @@ public class MainActivity extends AppCompatActivity {
     private Context mContext = MainActivity.this;
     private FragmentContainerView fragmentContainerView;
     private FloatingActionButton fabAddReminder;
-
+    private BottomAppBar bottomAppBar;
+    private NavController navController;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        fragmentContainerView = findViewById(R.id.fragmentContainer);
-        fabAddReminder = findViewById(R.id.fabAddReminder);
-
-        fabAddReminder.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                Navigation.findNavController(fragmentContainerView).navigate(R.id.addReminderFragment);
-                startActivity(new Intent(mContext, AddReminderActivity.class));
-            }
-        });
+        initViews();
 
         NavHostFragment navHostFragment =
                 (NavHostFragment) getSupportFragmentManager()
                         .findFragmentById(fragmentContainerView.getId());
-        NavController navController = Objects.requireNonNull(navHostFragment).getNavController();
+        navController = Objects.requireNonNull(navHostFragment).getNavController();
 
         navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
             @Override
             public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
-                if (destination.getLabel().equals("Add Reminder")){
-                    fabAddReminder.setVisibility(View.GONE);
+                if (Objects.requireNonNull(destination.getLabel()).equals("Home")){
                 }else {
                     fabAddReminder.setVisibility(View.VISIBLE);
                 }
+            }
+        });
+
+    }
+
+    private void initViews(){
+        fragmentContainerView = findViewById(R.id.fragmentContainer);
+        fabAddReminder = findViewById(R.id.fabAddReminder);
+        bottomAppBar = findViewById(R.id.bottom_app_bar);
+
+        fabAddReminder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Objects.requireNonNull(Objects.requireNonNull(navController.getCurrentDestination()).getLabel()).equals("Home")){
+                    Toast.makeText(mContext, "Add new medicine", Toast.LENGTH_SHORT).show();
+                }else if (Objects.equals(navController.getCurrentDestination().getLabel(), "Reminders")){
+                    startActivity(new Intent(mContext, AddReminderActivity.class));
+                }else if (navController.getCurrentDestination().getLabel().equals("Profile")){
+                    Toast.makeText(mContext, "Add new employee", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.menuHome:
+                        Navigation.findNavController(fragmentContainerView).navigate(R.id.homeFragment);
+                        break;
+                    case R.id.menuReminder:
+                        Navigation.findNavController(fragmentContainerView).navigate(R.id.remindersFragment);
+                        break;
+                    case R.id.menuProfile:
+                        Navigation.findNavController(fragmentContainerView).navigate(R.id.userProfileFragment);
+                        break;
+                }
+                return false;
             }
         });
 
---
+++ app/src/main/java/com/virtualstudios/medistore/ui/fragments/RemindersFragment.java
@@ -0,0 +1,36 @@
+package com.virtualstudios.medistore.ui.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.virtualstudios.medistore.R;
+import com.virtualstudios.medistore.ui.adapters.MedicineAdapter;
+
+public class RemindersFragment extends Fragment {
+
+    private View rootView;
+    private RecyclerView recyclerView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        if (rootView == null){
+            rootView = inflater.inflate(R.layout.fragment_reminders, container, false);
+            initViews();
+        }
+        return rootView;
+    }
+
+    private void initViews(){
+        recyclerView = rootView.findViewById(R.id.recyclerViewReminders);
+        recyclerView.setLayoutManager(new LinearLayoutManager(rootView.getContext()));
+        recyclerView.setAdapter(new MedicineAdapter());
+    }
+}
