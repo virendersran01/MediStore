cd57965422fe358a882c060730dc4535aae2ae84
virendersran01 <virendersran01@gmail.com>
2020-07-24 14:43:31 +0530

    Added new fragment for add medicine.

--- app/src/main/java/com/virtualstudios/medistore/ui/fragments/AddMedicineFragment.java
+++ app/src/main/java/com/virtualstudios/medistore/ui/fragments/AddMedicineFragment.java
@@ -2,6 +2,7 @@ package com.virtualstudios.medistore.ui.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -16,7 +17,7 @@ public class AddMedicineFragment extends Fragment {
     private View rootView;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         if (rootView == null){
             inflater.inflate(R.layout.fragment_add_medicine, container, false);
