f6e537b0ebfdd1b0fae0729307d4c5e736705255
virendersran01 <virendersran01@gmail.com>
2020-07-29 12:17:59 +0530

    working on add medicine.

--- app/src/main/java/com/virtualstudios/medistore/ui/fragments/AddMedicineFragment.java
+++ app/src/main/java/com/virtualstudios/medistore/ui/fragments/AddMedicineFragment.java
@@ -1,5 +1,6 @@
 package com.virtualstudios.medistore.ui.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.virtualstudios.medistore.R;
 
@@ -15,7 +18,9 @@ import com.virtualstudios.medistore.R;
 public class AddMedicineFragment extends Fragment {
 
     private View rootView;
-
+    private Spinner spinnerCategory, spinnerIsAthical;
+    private String[] categoryOptions= {"Allopathic", "Homeopathic", "Ayurvedic"};
+    private String[] isAthicalOptions= {"Yes", "No"};
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -29,5 +34,9 @@ public class AddMedicineFragment extends Fragment {
 
     private void initView(){
         rootView.findViewById(R.id.imageBack).setOnClickListener(view -> requireActivity().onBackPressed());
+        spinnerCategory = rootView.findViewById(R.id.spinnerCategory);
+        spinnerIsAthical = rootView.findViewById(R.id.spinnerIsAthical);
+        spinnerCategory.setAdapter(new ArrayAdapter<>(rootView.getContext(), android.R.layout.simple_list_item_1, categoryOptions));
+        spinnerIsAthical.setAdapter(new ArrayAdapter<>(rootView.getContext(), android.R.layout.simple_list_item_1, isAthicalOptions));
     }
 }
