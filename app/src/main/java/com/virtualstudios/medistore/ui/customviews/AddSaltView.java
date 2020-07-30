567c9d82824e3616818761cd0807c58e5948f9b8
virendersran01 <virendersran01@gmail.com>
2020-07-30 14:02:26 +0530

    working on add medicine.

--- app/src/main/java/com/virtualstudios/medistore/ui/customviews/AddSaltView.java
+++ app/src/main/java/com/virtualstudios/medistore/ui/customviews/AddSaltView.java
@@ -3,6 +3,8 @@ package com.virtualstudios.medistore.ui.customviews;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -11,12 +13,15 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import com.google.android.material.textfield.TextInputLayout;
 import com.virtualstudios.medistore.R;
 
+import java.util.Objects;
+
 public class AddSaltView extends ConstraintLayout {
 
     private TextInputLayout inputName;
     private TextInputLayout inputPotency;
     private Spinner spinnerPotencyType;
     private String[] potencyOptions = {"mg", "gm", "mcg"};
+    private String potencyTypeValue = null;
 
     public AddSaltView(Context context) {
         super(context);
@@ -39,5 +44,67 @@ public class AddSaltView extends ConstraintLayout {
         inputPotency = findViewById(R.id.inputPotency);
         spinnerPotencyType = findViewById(R.id.spinnerPotencyType);
         spinnerPotencyType.setAdapter(new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, potencyOptions));
+
+        spinnerPotencyType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                potencyTypeValue = potencyOptions[i];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
+
+    private String getName(){
+        return Objects.requireNonNull(inputName.getEditText()).getText().toString();
+    }
+
+    private String getPotency(){
+        return Objects.requireNonNull(inputPotency.getEditText()).getText().toString();
+    }
+
+    public String getPotencyTypeValue(){
+        if(potencyTypeValue != null){
+            return potencyTypeValue;
+        }
+        return null;
+    }
+
+    private boolean validSaltName(){
+        if (getName().isEmpty()){
+            Objects.requireNonNull(inputName.getEditText()).setError(getContext().getString(R.string.error_username_empty));
+            return false;
+        }else {
+            Objects.requireNonNull(inputName.getEditText()).setError(null);
+            return true;
+        }
+    }
+
+    private boolean validProtencyValue(){
+        if (getPotency().isEmpty()){
+            Objects.requireNonNull(inputPotency.getEditText()).setError(getContext().getString(R.string.error_username_empty));
+            return false;
+        }else {
+            Objects.requireNonNull(inputPotency.getEditText()).setError(null);
+            return true;
+        }
+    }
+
+    public String getSaltName(){
+        if (validSaltName()){
+            return getName();
+        }
+        return null;
+    }
+
+    public String getProtencyValue(){
+        if (validProtencyValue()){
+            return getPotency();
+        }
+        return null;
+    }
+
 }
--- app/src/main/java/com/virtualstudios/medistore/ui/fragments/AddMedicineFragment.java
+++ app/src/main/java/com/virtualstudios/medistore/ui/fragments/AddMedicineFragment.java
@@ -5,13 +5,19 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.Spinner;
 
+import com.google.android.material.button.MaterialButton;
 import com.virtualstudios.medistore.R;
+import com.virtualstudios.medistore.ui.customviews.AddSaltView;
 
 
 public class AddMedicineFragment extends Fragment {
@@ -21,6 +27,10 @@ public class AddMedicineFragment extends Fragment {
     private String[] categoryOptions= {"Allopathic", "Homeopathic", "Ayurvedic"};
     private String[] isAthicalOptions= {"Yes", "No"};
     private String[] typeOptions= {"Tablet", "Capsule", "Syrup"};
+    private LinearLayout layoutAddSalts;
+    private AddSaltView addSaltView;
+    private ImageView imageAddSalt;
+    private MaterialButton buttonAdd;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -40,5 +50,28 @@ public class AddMedicineFragment extends Fragment {
         spinnerCategory.setAdapter(new ArrayAdapter<>(rootView.getContext(), android.R.layout.simple_list_item_1, categoryOptions));
         spinnerIsAthical.setAdapter(new ArrayAdapter<>(rootView.getContext(), android.R.layout.simple_list_item_1, isAthicalOptions));
         spinnerType.setAdapter(new ArrayAdapter<>(rootView.getContext(), android.R.layout.simple_list_item_1, typeOptions));
+        layoutAddSalts = rootView.findViewById(R.id.layoutAddSalt);
+        addSaltView = layoutAddSalts.findViewById(R.id.viewAddSalt);
+        imageAddSalt = rootView.findViewById(R.id.imageAddSalt);
+        buttonAdd = rootView.findViewById(R.id.buttonAdd);
+
+        imageAddSalt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AddSaltView addSaltView1 = new AddSaltView(rootView.getContext());
+                LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                layoutParams.setMargins(0, 10, 0,0);
+                addSaltView1.setLayoutParams(layoutParams);
+                layoutAddSalts.addView(addSaltView1);
+
+            }
+        });
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("TAG", "salt : "+addSaltView.getSaltName() + " potency : "+addSaltView.getProtencyValue() + " "+addSaltView.getPotencyTypeValue());
+            }
+        });
     }
 }
